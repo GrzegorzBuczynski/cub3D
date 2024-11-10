a36da66f54fe1f3b6df9f25ded506f0151e7fbeb
GrzegorzBuczynski <grzegorzbuczynski93@gmail.com>
2024-11-10 21:09:33 +0100

    make push!

--- src/display_server/colors.c
+++ src/display_server/colors.c
@@ -6,7 +6,7 @@
 /*   By: gbuczyns <gbuczyns@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 17:01:09 by gbuczyns          #+#    #+#             */
-/*   Updated: 2024/11/10 19:42:29 by gbuczyns         ###   ########.fr       */
+/*   Updated: 2024/11/10 20:56:26 by gbuczyns         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ int	get_color(t_vector current, t_vector start, t_vector end, t_vector delta)
 	// int		green;
 	// int		blue;
 	// double	percentage;
-
 	// if (current.color == end.color)
 	// 	return (current.color);
 	// if (delta.x > delta.y)
@@ -47,4 +46,24 @@ int	get_color(t_vector current, t_vector start, t_vector end, t_vector delta)
 	// blue = get_light(start.color & 0xFF, end.color & 0xFF, percentage);
 	// return ((red << 16) | (green << 8) | blue);
 	return (COLOR_CYAN);
+}
+
+int	parse_color(char *str)
+{
+	char *temp;
+	int red;
+	int green;
+	int blue;
+
+	temp = str;
+	ft_skip_whitespace(&temp);
+	red = ft_atoi(temp);
+	temp = ft_strchr(temp, ',');
+	temp++;
+	green = ft_atoi(temp);
+	temp = ft_strchr(temp, ',');
+	temp++;
+	blue = ft_atoi(temp);
+
+	return ((red << 16) | (green << 8) | blue);
 }
--- src/display_server/walls.c
+++ src/display_server/walls.c
@@ -6,17 +6,34 @@
 /*   By: gbuczyns <gbuczyns@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 18:31:28 by gbuczyns          #+#    #+#             */
-/*   Updated: 2024/11/10 20:21:34 by gbuczyns         ###   ########.fr       */
+/*   Updated: 2024/11/10 21:09:27 by gbuczyns         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
 
-
-
-
-void print_wall(t_game *data)
+void	print_wall(t_game *data)
 {
-	draw_line((t_vector){0, 0}, (t_vector){SCREEN_WIDTH, SCREEN_HEIGHT}, &data->display);
-	
+	int lines_per_degree;
+	int i = 0;
+	int j = 0;
+
+	lines_per_degree = SCREEN_WIDTH / FOV;
+
+	while (i < SCREEN_WIDTH/lines_per_degree)
+	{
+		j = 0;
+		while (j < lines_per_degree)
+		{
+			draw_line((t_vector){(i * lines_per_degree )+ j, (SCREEN_HEIGHT/2) -50}, (t_vector){(i * lines_per_degree )+ j, (SCREEN_HEIGHT/2) +50},
+				&data->display);
+			j++;
+		}
+		// Calculate the angle of the ray
+		// Calculate the distance to the wall
+		// Calculate the height of the wall
+		// Calculate the color of the wall
+		// Draw the wall
+		i++;
+	}
 }
