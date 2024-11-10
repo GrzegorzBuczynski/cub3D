43c27bb4081fed761650f4b5257384f120b6abe5
GrzegorzBuczynski <grzegorzbuczynski93@gmail.com>
2024-11-10 21:34:41 +0100

    make push!

--- src/display_server/walls.c
+++ src/display_server/walls.c
@@ -6,7 +6,7 @@
 /*   By: gbuczyns <gbuczyns@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 18:31:28 by gbuczyns          #+#    #+#             */
-/*   Updated: 2024/11/10 21:09:27 by gbuczyns         ###   ########.fr       */
+/*   Updated: 2024/11/10 21:34:38 by gbuczyns         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,41 @@
 
 void	print_wall(t_game *data)
 {
-	int lines_per_degree;
-	int i = 0;
-	int j = 0;
+	float	degree;
+	int		i;
+	int		j;
 
-	lines_per_degree = SCREEN_WIDTH / FOV;
-
-	while (i < SCREEN_WIDTH/lines_per_degree)
+	i = 0;
+	j = 0;
+	while (i <= SCREEN_WIDTH)
 	{
-		j = 0;
-		while (j < lines_per_degree)
-		{
-			draw_line((t_vector){(i * lines_per_degree )+ j, (SCREEN_HEIGHT/2) -50}, (t_vector){(i * lines_per_degree )+ j, (SCREEN_HEIGHT/2) +50},
-				&data->display);
-			j++;
-		}
 		// Calculate the angle of the ray
+		degree = (-FOV / 2) + ((FOV / SCREEN_WIDTH) * i);
+		printf("degree: %f\n", degree);
 		// Calculate the distance to the wall
 		// Calculate the height of the wall
 		// Calculate the color of the wall
 		// Draw the wall
+		draw_line((t_vector){i, (SCREEN_HEIGHT / 2) - 50}, (t_vector){i,
+			(SCREEN_HEIGHT / 2) + 50}, &data->display);
 		i++;
 	}
 }
+
+// while (i < SCREEN_WIDTH/lines_per_degree)
+// {
+// 	j = 0;
+// 	while (j < lines_per_degree)
+// 	{
+// 		draw_line((t_vector){(i * lines_per_degree )+ j, (SCREEN_HEIGHT/2) -50},
+// (t_vector){(i * lines_per_degree )+ j, (SCREEN_HEIGHT/2) +50},
+// 			&data->display);
+// 		j++;
+// 	}
+// 	// Calculate the angle of the ray
+// 	// Calculate the distance to the wall
+// 	// Calculate the height of the wall
+// 	// Calculate the color of the wall
+// 	// Draw the wall
+// 	i++;
+// }
