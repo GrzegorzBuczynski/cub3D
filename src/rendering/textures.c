61fb55d5ed9a772c83e11e54648a95e4f7f10f99
GrzegorzBuczynski <grzegorzbuczynski93@gmail.com>
2024-12-03 14:32:25 +0100

    make push!

--- src/rendering/textures.c
+++ src/rendering/textures.c
@@ -6,13 +6,13 @@
 /*   By: gbuczyns <gbuczyns@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 17:13:11 by gbuczyns          #+#    #+#             */
-/*   Updated: 2024/12/03 14:29:59 by gbuczyns         ###   ########.fr       */
+/*   Updated: 2024/12/03 14:32:05 by gbuczyns         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D.h"
 
-int	get_texture_pixel(t_image *texture, double tex_x, double tex_y)
+int	get_texture_pixel(t_image *texture, double tex_y, double tex_x)
 {
 	int				offset;
 	unsigned int	*pixel;
@@ -22,8 +22,8 @@ int	get_texture_pixel(t_image *texture, double tex_x, double tex_y)
 	if (!texture || !texture->pixel_data || tex_x < 0 || tex_y < 0 || tex_x > 1
 		|| tex_y > 1)
 		return (0);
-	x = tex_x * texture->width;
-	y = tex_y * texture->height;
+	x = (int)tex_x * texture->width;
+	y = (int)tex_y * texture->height;
 	offset = y * texture->line_length + x * (texture->bpp / 8);
 	pixel = (unsigned int *)(texture->pixel_data + offset);
 	return (*pixel);
