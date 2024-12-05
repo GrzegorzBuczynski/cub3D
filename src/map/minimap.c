1677083100761de681ac3b38b252f1e71f6226fa
GrzegorzBuczynski <grzegorzbuczynski93@gmail.com>
2024-12-05 19:01:12 +0100

    make push!

--- src/map/minimap.c
+++ src/map/minimap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minimap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ssuchane <ssuchane@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gbuczyns <gbuczyns@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 18:10:52 by ssuchane          #+#    #+#             */
-/*   Updated: 2024/12/03 20:08:23 by ssuchane         ###   ########.fr       */
+/*   Updated: 2024/12/05 19:00:00 by gbuczyns         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,9 +44,19 @@ void	draw_player(t_image *image, int y, int x)
 	draw_square(image, y, x, PLAYER_COLOR);
 }
 
-void	draw_wall(t_image *image, int y, int x)
+void	draw_wall2(t_image *image, int y, int x)
 {
 	draw_square(image, y, x, WALL_COLOR);
+}
+
+void	draw_wall(t_image *image, t_vector on_screen_pos, t_vector on_map_pos)
+{
+	if (on_map_pos.x > 64)
+	{
+		printf("on_map_pos.y: %d\n", on_map_pos.y);
+	}
+	draw_square(image, on_screen_pos.y + on_map_pos.y * MINIMAP_SCALE,
+		on_screen_pos.x + on_map_pos.x * MINIMAP_SCALE, WALL_COLOR);
 }
 
 void	draw_floor(t_image *image, int y, int x)
@@ -74,8 +84,119 @@ void	draw_minimap_background(t_image *image, int y, int x,
 	}
 }
 
+void	get_minimap_color(t_game *game, t_vector sm_pos, t_vector center)
+{
+	t_vector	mini_grid;
+
+	mini_grid.x = center.x - PLAYER_RADIUS + sm_pos.x;
+	mini_grid.y = center.y - PLAYER_RADIUS + sm_pos.y;
+	if (mini_grid.y >= 0 && mini_grid.y < game->map.height && mini_grid.x >= 0
+		&& mini_grid.x < ft_strlen(game->map.grid[mini_grid.y]))
+	{
+		if (game->map.grid[mini_grid.y][mini_grid.x] == '1')
+			draw_wall2(&game->display.img, sm_pos.y * MINIMAP_SCALE
+				+ MINIMAP_PADDING_Y, sm_pos.x * MINIMAP_SCALE
+				+ MINIMAP_PADDING_X);
+	}
+	// else
+	// 	draw_floor(&game->display.img, sm_pos.y * MINIMAP_SCALE
+	// 		+ MINIMAP_PADDING_Y, sm_pos.x * MINIMAP_SCALE + MINIMAP_PADDING_X);
+}
+
+void	update_center(t_vector *center, t_game *game, int y)
+{
+	int	i;
+	int len;
+
+	center->x = game->player.pos.x;
+	center->y = game->player.pos.y;
+	if (center->x < PLAYER_RADIUS)
+		center->x = PLAYER_RADIUS;
+	if (y > game->map.height)
+		len = game->map.width;
+	else
+		len = ft_strlen(game->map.grid[y]);
+	if (center->x > len - PLAYER_RADIUS)
+		center->x = len - PLAYER_RADIUS;
+	if (center->y > game->map.height - PLAYER_RADIUS)
+		center->y = game->map.height - PLAYER_RADIUS;
+	if (center->y < PLAYER_RADIUS)
+		center->y = PLAYER_RADIUS;
+}
+
+void	draw_minimap2(t_game *game)
+{
+	t_vector	sm_pos;
+	t_vector	center;
+
+	sm_pos.y = 0;
+	while (sm_pos.y < 2 * PLAYER_RADIUS + 1)
+	{
+		sm_pos.x = 0;
+		while (sm_pos.x < 2 * PLAYER_RADIUS + 1)
+		{
+			update_center(&center, game, sm_pos.y);
+			get_minimap_color(game, sm_pos, center);
+			sm_pos.x++;
+		}
+		sm_pos.y++;
+	}
+}
+
+void	draw_walls(t_game *game, t_vector m_pos_on_screen, t_vector size,
+		t_vector center)
+{
+	int			y;
+	int			x;
+	t_vector	sm_pos;
+	t_image		*image;
+	t_vector	pos_on_grid;
+
+	image = &game->display.img;
+	pos_on_grid.y = center.y - PLAYER_RADIUS;
+	sm_pos.y = 0;
+	while (pos_on_grid.y < game->map.height && pos_on_grid.y < center.y
+		+ PLAYER_RADIUS)
+	{
+		pos_on_grid.x = center.x - PLAYER_RADIUS;
+		sm_pos.x = 0;
+		while (pos_on_grid.x < ft_strlen(game->map.grid[pos_on_grid.y])
+			&& pos_on_grid.x < center.x + PLAYER_RADIUS)
+		{
+			if (game->map.grid[pos_on_grid.y][pos_on_grid.x] == '1')
+			{
+				draw_wall(image, m_pos_on_screen, sm_pos);
+			}
+			pos_on_grid.x++;
+			sm_pos.x++;
+		}
+		pos_on_grid.y++;
+		sm_pos.y++;
+	}
+}
+
 void	draw_minimap(t_game *game)
 {
+	t_vector m_pos_on_screen; // on screen position
+	t_vector center;          // map center point position
+	t_vector size;            // size of the minimap
+	t_vector p_pos_on_screen; // player position
+	m_pos_on_screen.x = 100;
+	m_pos_on_screen.y = 100;
+	center.x = game->player.pos.x;
+	center.y = game->player.pos.y;
+	size.x = (2 * PLAYER_RADIUS + 1) * MINIMAP_SCALE;
+	size.y = (2 * PLAYER_RADIUS + 1) * MINIMAP_SCALE;
+	p_pos_on_screen.x = m_pos_on_screen.x + size.x / 2;
+	p_pos_on_screen.y = m_pos_on_screen.y + size.y / 2;
+	draw_minimap_background(&game->display.img, m_pos_on_screen.y,
+	m_pos_on_screen.x, FLOOR_COLOR);
+	draw_minimap2(game);
+	// draw_walls2(game, m_pos_on_screen, size, center);
+	draw_player(&game->display.img, p_pos_on_screen.y, p_pos_on_screen.x);
+}
+
+/*
 	int			y;
 	int			x;
 	t_vector	p_pos;
@@ -110,5 +231,4 @@ void	draw_minimap(t_game *game)
 		y++;
 		m_pos.y++;
 	}
-	draw_player(&game->display.img, 100, 100);
-}
+ */
